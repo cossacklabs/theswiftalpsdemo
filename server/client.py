a65433c98f6161470bf921938a51dbbef7aa9055
andrey <andrey@cossacklabs.com>
2016-11-09 00:10:47 +0200

    Add serer and client

---
+++ server/client.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3.5
+import http.client, urllib.parse, http.cookies
+import base64
+import json
+import random
+import string
+import argparse
+
+
+from pythemis import ssession
+from pythemis import skeygen
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-u', '--url', help='ip', default="127.0.0.1")
+parser.add_argument('-p', '--port', help='port', type=int, default=8181)
+parser.add_argument('-s', '--server_pub_key', help='server public key')
+args = parser.parse_args()
+port = args.port
+
+url = args.url
+port = args.port
+server_id = 'server'
+server_pub_key = args.server_pub_key
+messages = ["message1", "message2", "message3", "message4", "message5", "message6", "message7", "message8", "message9", "message10"]
+
+id_symbols = string.ascii_letters + string.digits
+def generate_str(len):
+    return ''.join([random.choice(id_symbols) for _ in range(len)])
+
+client_name = generate_str(10)
+for i in range(0,99):
+    messages.append(generate_str(48))
+
+key_pair=skeygen.themis_gen_key_pair('EC')
+client_private_key=key_pair.export_private_key()
+client_public_key=key_pair.export_public_key()
+
+class Transport(ssession.mem_transport):
+    def get_pub_key_by_id(self, user_id):
+        if user_id == b'server':
+            return base64.urlsafe_b64decode(server_pub_key)
+
+
+params = urllib.parse.urlencode({'client_name': client_name, 'public_key': base64.urlsafe_b64encode(client_public_key)})
+headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+conn = http.client.HTTPConnection(url, port)
+conn.request("POST", "/connect_request", params, headers)
+response = conn.getresponse()
+if response.status == 200:
+    print(response.read())
+    print(response.getheaders())
+    
+    session = ssession.ssession(client_name.encode("UTF-8"), client_private_key, Transport())
+    msg = session.connect_request()
+    while True:
+        params = urllib.parse.urlencode({'message': base64.urlsafe_b64encode(msg)})
+        conn.request("POST", "/message", params, headers)
+        response = conn.getresponse()
+        if response.status == 200:
+            msg = response.read()
+            print(msg)
+            print(response.getheaders())
+            cookie = http.cookies.SimpleCookie()
+            headers["Cookie"] = ""
+            for header in response.getheaders():
+                if header[0] == 'SET-COOKIE':
+                    cookie.load(header[1])
+                    headers["Cookie"] = "session_id="+cookie['session_id'].value
+            msg = session.unwrap(base64.urlsafe_b64decode(msg))
+            print(msg)
+            if not msg.is_control:
+                break
+
+    for mess in messages:
+        m = {'name': client_name, 'msg': mess}
+        params = urllib.parse.urlencode({'message': base64.urlsafe_b64encode(session.wrap(json.dumps(m).encode("UTF-8")))})
+        conn.request("POST", "/message", params, headers)
+        response = conn.getresponse()
+        if response.status != 200:
+            break;
+        print(response.read())
+
+conn.close()
---
+++ server/server.py
@@ -0,0 +1,140 @@
+#!/usr/bin/python3.5
+
+import argparse
+import logging
+import random
+import string
+import asyncio
+import base64
+import time
+import datetime
+import uuid
+import jinja2
+import json
+from aiohttp import web
+import aiohttp_jinja2
+
+from pythemis import ssession
+from pythemis import skeygen
+
+class Transport(ssession.mem_transport):
+    def get_pub_key_by_id(self, user_id):
+        print(user_id)
+        return base64.urlsafe_b64decode(pub_keys[user_id.decode("utf-8")]['pub_key'])
+
+
+@asyncio.coroutine
+@aiohttp_jinja2.template('index.html')
+def index(request):
+    url = '{scheme}://{host}/'.format(scheme="http", host=request.host)
+    return {'url': url, 'server_id': 'server', 'server_public_key': base64.urlsafe_b64encode(server_public_key).decode("UTF-8")}
+
+@asyncio.coroutine
+def get_new_messages(request):
+    global history
+#    if len(history) = 0:
+#        text = "{\"error\" : \"<img src='https://mokum.place/system/attachments/000/178/129/mokum-medium-4965ce7a2f478e4f4a6ca07cd76b759300d17a9c.jpg'>\"}"
+#    else:
+#       print(123)
+    text = "{\"info\": "+json.dumps(history) + "}"
+    history = []
+    return web.Response(text=text)    
+
+@asyncio.coroutine
+def register_new_user(request):
+    data = yield from request.post()
+    if(data['client_name'] in pub_keys):
+        return web.Response(status=500, text="User with name <b>"+data['client_name']+"</b> already exist")
+    else:
+        print(data['client_name'], data['public_key'])
+        pub_keys[data['client_name']] = {'pub_key': data['public_key'], 'time': time.time()}
+        return web.Response(text="User <b>"+data['client_name']+"</b> registered successfully")
+
+@asyncio.coroutine
+def message(request):
+    global sessions
+    global history
+    data = yield from request.post()
+    if 'message' not in data:
+        return web.Response(status=500, text="incorrect request")
+    if 'session_id' not in request.cookies:
+        session = ssession.ssession(b'server', server_private_key, Transport());
+        msg = session.unwrap(base64.urlsafe_b64decode(data['message']))
+        if msg.is_control:
+            session_id = str(uuid.uuid4())
+            sessions[session_id] = {'start': time.time(), 'last': time.time(), 'session': session}
+            resp = web.Response(text=base64.urlsafe_b64encode(msg).decode("UTF-8"));
+            resp.set_cookie("session_id", session_id)
+            return resp
+    else:
+        session_id = request.cookies['session_id']
+        session = sessions[session_id]['session']
+        msg = session.unwrap(base64.urlsafe_b64decode(data['message']))
+        print(msg)        
+        if msg.is_control:
+            sessions[session_id]['last'] = time.time()
+            resp = web.Response(text=base64.urlsafe_b64encode(msg).decode("UTF-8"));
+            resp.set_cookie("session_id", session_id)
+            return resp
+        else:
+            m = json.loads(msg.decode("UTF-8"))
+            history.append({'name': m['name'], 'time': datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'), 'message': m['msg']})
+            return web.Response(text="Ok")
+                           
+
+@asyncio.coroutine
+def init(port, loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    app.router.add_route('GET', '/get_new_messages', get_new_messages)
+    app.router.add_route('POST', '/connect_request', register_new_user)
+    app.router.add_route('POST', '/message', message)
+
+
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('templates/'))
+
+    handler = app.make_handler()
+    srv = yield from loop.create_server(handler, '0.0.0.0', port)
+    logger.info("Server started at http://0.0.0.0:{}".format(port))
+    return handler, app, srv
+
+
+@asyncio.coroutine
+def finish(app, srv, handler):
+    global online
+    for sockets in online.values():
+        for socket in sockets:
+            socket.close()
+
+    yield from asyncio.sleep(0.1)
+    srv.close()
+    yield from handler.finish_connections()
+    yield from srv.wait_closed()
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Run server')
+
+    parser.add_argument('-p', '--port', type=int, help='Port number', default=8181)
+    parser.add_argument('-v', '--verbose', action='store_true', help='Output verbosity')
+    args = parser.parse_args()
+    port = args.port
+
+    logging.basicConfig(level=logging.INFO if args.verbose else logging.WARNING)
+    logger = logging.getLogger(__name__)
+
+    key_pair=skeygen.themis_gen_key_pair('EC')
+    server_private_key=key_pair.export_private_key()
+    server_public_key=key_pair.export_public_key()
+    
+    pub_keys = {}
+    sessions = {}
+    history = []
+    loop = asyncio.get_event_loop()
+    handler, app, srv = loop.run_until_complete(init(port, loop))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.run_until_complete(finish(app, srv, handler))
